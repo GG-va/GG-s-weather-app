bf6bcc50eb73798ed27be146fdb3613e58bcf063
GG-va <127759678+GG-va@users.noreply.github.com>
2023-03-20 12:13:32 -0400

    Updated the time

--- JS/index.js
+++ JS/index.js
@@ -1,59 +1,73 @@
+
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    let days = [
+        "Sun",
+        "Mon",
+        "Tue",
+        "Wed",
+        "Thu",
+        "Fri",
+        "Sat"
+    ];
+    let day = days[date.getDay()];
+    let months = [
+        "Jan",
+        "Feb",
+        "March",
+        "April",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec"
+    ];
+    let month = months[date.getMonth()];
+    return `${date}`;
+}
+
+
 function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
+    let dateElement = document.querySelector("#date");
 
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
+    dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "b7d735a7d28a342e84165d7055229f3f";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 
 axios.get(apiUrl).then(displayTemperature);
 
 
-let now = new Date();
-let days = [
-    "Sun",
-    "Mon",
-    "Tue",
-    "Wed",
-    "Thu",
-    "Fri",
-    "Sat"
-];
-let months = [
-    "Jan",
-    "Feb",
-    "March",
-    "April",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec"
-];
 
-let hours = now.getHours();
-if (hours < 10) {
-    hours = `0${hours}`;
-}
-let minutes = now.getMinutes();
-if (minutes < 10) {
-    minutes = `0${minutes}`;
-}
-let date = now.getDate();
-let day = days[now.getDay()];
-let month = months[now.getMonth()];
+
+
+
+
+
+
 let h3 = document.querySelector("h3");
 h3.innerHTML = `${day} ${month} ${date}, ${hours}:${minutes}`;
